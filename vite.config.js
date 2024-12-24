31f643500f49d98855b4aabce1f058cd1bb5b892
AeriMist <71417547+AeriMist@users.noreply.github.com>
2024-12-24 11:39:33 +0800

    Updated config and fixed image paths

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Flipcard", 
+  base: '/Flipcard/', // Adjust this if the deployment URL is different
+  build: {
+    rollupOptions: {
+      output: {
+        // Ensure assets are copied to the right directory when building
+        assetFileNames: 'assets/[name]-[hash][extname]'
+      }
+    }
+  }
 });
