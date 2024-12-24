dff9c4fdc166917d7dbd3b9e55bda2a43ebb076a
AeriMist <71417547+AeriMist@users.noreply.github.com>
2024-12-24 12:01:31 +0800

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Flipcard', // Adjust this if the deployment URL is different
+  base: '/Flipcard/', // Adjust this if the deployment URL is different
   build: {
     rollupOptions: {
       output: {
